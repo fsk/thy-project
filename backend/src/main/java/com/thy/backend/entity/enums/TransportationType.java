f9db53bf20efc48c334eaa7db54d520a6828d887
Furkan Şahin Kulaksız <37078155+fsk@users.noreply.github.com>
2026-04-02 22:17:36 +0300

    fake data generator created (#3)

--- backend/src/main/java/com/thy/backend/entity/enums/TransportationType.java
+++ backend/src/main/java/com/thy/backend/entity/enums/TransportationType.java
@@ -1,11 +1,8 @@
 package com.thy.backend.entity.enums;
 
 public enum TransportationType {
-    BUS,
-    TRAIN,
     FLIGHT,
-    FERRY,
-    METRO,
-    TAXI,
-    OTHER
+    BUS,
+    SUBWAY,
+    UBER
 }
